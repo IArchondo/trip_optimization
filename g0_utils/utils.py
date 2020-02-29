fe5da0f0e8ae17192198ad763e305da72697d4bf
Ignacio Archondo <archondo.ignacio@bcg.com>
2020-02-29 21:40:36 +0100

    Created function to load standard date from config

---
+++ g0_utils/utils.py
@@ -0,0 +1,28 @@
+import yaml
+from datetime import datetime
+from pathlib import Path
+
+
+def load_standard_date():
+    """Loads standard date for google maps queries
+        from config file
+    
+    Returns:
+        datetime.datetime: standard datetime
+    """
+
+    with open(Path("g0_utils/config.yml")) as file:
+        CONFIG = yaml.full_load(file)
+
+    standard_date_config = CONFIG["standard_time"]
+
+    standard_date = datetime(
+        year=standard_date_config["year"],
+        month=standard_date_config["month"],
+        day=standard_date_config["day"],
+        hour=standard_date_config["hour"],
+        mnute=standard_date_config["minute"],
+    )
+
+    return standard_date
+
