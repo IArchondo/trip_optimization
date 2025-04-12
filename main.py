754deee1f45b050d8db909621bd472fa77a2ee2a
git config --system -–edit <ignacio.archondo.contreras@alliander.com>
2025-04-12 19:12:12 +0100

    Fix visualizer

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-"""
-Main run script.
-"""
-import os
-import pandas as pd
-import pickle
-import logging
-import yaml
-from shutil import copy as copy_file
-from pathlib import Path
-from datetime import datetime
+"""Main run script."""
 
-from sched_opt.g0_utils.utils import load_problem_definition
-from sched_opt.g1_distance_calculation.DistanceMatrixCalculator import (
+import logging
+import os
+import pickle
+from datetime import datetime
+from pathlib import Path
+from shutil import copy as copy_file
+
+import pandas as pd
+import yaml
+
+from sched_opt.data_processing.data_processor import DataProcessor
+from sched_opt.distance_calculation.distance_matrix_calculator import (
     DistanceMatrixCalculator,
 )
-from sched_opt.g2_data_processing.DataProcessor import DataProcessor
-from sched_opt.g3_schedule_optimization.ScheduleOptimizer import ScheduleOptimizer
-from sched_opt.g4_schedule_visualization.ScheduleVisualizer import ScheduleVisualizer
+from sched_opt.g0_utils.utils import load_problem_definition
+from sched_opt.schedule_optimization.schedule_optimizer import ScheduleOptimizer
+from sched_opt.schedule_visualization.schedule_visualizer import ScheduleVisualizer
 
 logging.basicConfig(level=logging.INFO)
 
@@ -24,61 +24,52 @@ LOGGER = logging.getLogger("MainExecution")
 
 
 with open(Path("config.yml")) as file:
-    CONFIG = yaml.full_load(file)
+    config = yaml.full_load(file)
 
-EXECUTION_PIPELINE = CONFIG["execution_pipeline"]
+execution_pipeline = config["execution_pipeline"]
 
 
 if __name__ == "__main__":
+    current_run = str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
 
-    CURRENT_RUN = str(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
-
-    if EXECUTION_PIPELINE["fetch_distances"]:
-
-        input_data = pd.read_excel("data_input.xlsx")
+    if execution_pipeline["fetch_distances"]:
+        input_data = pd.read_excel("data_input copy.xlsx")
 
         dmc = DistanceMatrixCalculator(input_data)
 
-        DISTANCES = dmc.execute_pipeline(True)
+        distances = dmc.execute_pipeline(True)
 
-        with open(
-            Path("00_saved_data/saved_distances") / "latest_distances_2.pickle", "wb"
-        ) as handle:
-            pickle.dump(DISTANCES, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        with open(Path("00_saved_data/saved_distances") / "latest_distances_2.pickle", "wb") as handle:
+            pickle.dump(distances, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     else:
         LOGGER.info("Loading saved distances")
-        with open(
-            Path("00_saved_data/saved_distances") / "latest_distances_2.pickle", "rb"
-        ) as handle:
-            DISTANCES = pickle.load(handle)
+        with open(Path("00_saved_data/saved_distances") / "latest_distances_2.pickle", "rb") as handle:
+            distances = pickle.load(handle)
 
-    data_processor = DataProcessor(DISTANCES)
+    data_processor = DataProcessor()
 
-    PROC_DISTANCES = data_processor.execute_pipeline()
+    proc_distances = data_processor.execute_pipeline(distances)
 
-    PROBLEM_DEFINITION = load_problem_definition()
+    problem_definition = load_problem_definition()
 
-    SCHED_OPT = ScheduleOptimizer(PROC_DISTANCES, PROBLEM_DEFINITION)
+    sched_opt = ScheduleOptimizer(proc_distances, problem_definition)
 
-    SOLUTION_DICT = SCHED_OPT.execute_optimizer_pipeline()
+    solution_dict = sched_opt.execute_optimizer_pipeline()
 
-    SCHED_VIS = ScheduleVisualizer(PROC_DISTANCES, SOLUTION_DICT)
-    os.makedirs(f"02_reports/{CURRENT_RUN}")
+    sched_vis = ScheduleVisualizer(proc_distances, solution_dict)
+    os.makedirs(f"02_reports/{current_run}")
 
-    SCHED_VIS.execute_visualizer_pipeline(CURRENT_RUN)
+    sched_vis.execute_visualizer_pipeline(current_run)
 
     copy_file(
         "03_notebook_templates/trip_report_template.ipynb",
-        f"02_reports/{CURRENT_RUN}/trip_report_{CURRENT_RUN}.ipynb",
+        f"02_reports/{current_run}/trip_report_{current_run}.ipynb",
     )
 
     os.system(
-        (
-            "jupyter nbconvert --execute --no-input --no-prompt --to html "
-            + f"02_reports/{CURRENT_RUN}/trip_report_{CURRENT_RUN}.ipynb"
-        )
+        "jupyter nbconvert --execute --no-input --no-prompt --to html "
+        + f"02_reports/{current_run}/trip_report_{current_run}.ipynb"
     )
 
-    os.remove(f"02_reports/{CURRENT_RUN}/trip_report_{CURRENT_RUN}.ipynb")
-
+    os.remove(f"02_reports/{current_run}/trip_report_{current_run}.ipynb")
